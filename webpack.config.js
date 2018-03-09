96271758db428304fda89483b6cf0d8df645a9c7
tianaba95 <tatiana.barrios@takcolombia.com>
2018-03-09 10:38:45 -0500

    Webpack conf

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,37 @@
 var webpack = require('webpack');
 
 module.exports = {
-    entry: './src/main.ts',
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js', '.jsx']
+    },
+    entry: {
+        'app': './src/main.ts'
+      },
     module: {
-        rules: [{
-            test: /\.scss$/,
-            use: [{
-                loader: "style-loader"
-            }, {
-                loader: "css-loader"
-            }, {
-                loader: "sass-loader",
-                options: {
-                    includePaths: [		
-                        'node_modules/@angular-mdl/popover',
-                        'node_modules/@angular-mdl/select'
-                    ]
-                }
-            }]
+        rules: [
+            {
+                test: /\.ts$/,
+                use: [
+                    {
+                        loader: 'awesome-typescript-loader'
+                    }
+                ]
+            },
+            {
+                test: /\.scss$/,
+                use: [{
+                    loader: "style-loader"
+                }, {
+                    loader: "css-loader"
+                }, {
+                    loader: "sass-loader",
+                    options: {
+                        includePaths: [		
+                            'node_modules/@angular-mdl/popover',
+                            'node_modules/@angular-mdl/select'
+                        ]
+                    }
+                }]
         }]
     },
     output: {
